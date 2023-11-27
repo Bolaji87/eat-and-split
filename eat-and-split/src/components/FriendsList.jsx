4f387dd35259ecd22d639863fc6b4ec3e740eaea
bolaji87 <sakabolaji77@gmail.com>
2023-11-28 00:37:35 +0100

    add FriendsList component

---
+++ eat-and-split/src/components/FriendsList.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+function FriendsList({ friends }) {
+	return (
+		<ul>
+			{friends.map((friend) => (
+				<Friend key={friend.id} friend={friend} />
+			))}
+		</ul>
+	);
+}
+
+function Friend({ friend }) {
+	return (
+		<li>
+			<img src={friend.image} alt={friend.name} />
+			<h3>{friend.name}</h3>
+
+			{friend.balance < 0 && (
+				<p className="red">
+					You owe {friend.name} $ {Math.abs(friend.balance)}
+				</p>
+			)}
+			{friend.balance > 0 && (
+				<p className="green">
+					{friend.name} owes you $ {friend.balance}
+				</p>
+			)}
+			{friend.balance === 0 && <p>You and {friend.name} are even</p>}
+
+			<button className="button">Select</button>
+		</li>
+	);
+}
+
+export default FriendsList;
