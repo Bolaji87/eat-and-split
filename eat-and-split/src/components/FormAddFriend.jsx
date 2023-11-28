c57bb940d38b67edcc4c1efb69e3f6f697f5a2d6
bolaji87 <sakabolaji77@gmail.com>
2023-11-28 01:08:29 +0100

    add FormAddFriend component

---
+++ eat-and-split/src/components/FormAddFriend.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Button from "./Button.jsx";
+
+function FormAddFriend() {
+	return (
+		<form className="form-add-friend">
+			<label>👬 Friend name</label>
+			<input type="text" />
+
+			<label>🖼 Image URL</label>
+			<input type="text" />
+
+			<Button>Add</Button>
+		</form>
+	);
+}
+
+export default FormAddFriend;
